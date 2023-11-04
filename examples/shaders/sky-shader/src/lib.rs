36852ceb3fe1d99663661422f47bcaba307b6246
Ethan Almloff <76850177+NoodlesOfWrath@users.noreply.github.com>
2023-11-04 18:31:41 -0500

    An attempt to debug has been made

--- examples/shaders/sky-shader/src/lib.rs
+++ examples/shaders/sky-shader/src/lib.rs
@@ -3,9 +3,9 @@
 #![deny(warnings)]
 
 use shared::glam::{vec2, vec4, Vec2, Vec4};
-use shared::{Circle, CirclesToDisplay};
+use shared::{Circle, CirclesToDisplay, Vec2Homebrew};
 use spirv_std::spirv;
-const NUM_CIRCLES: i32 = 32;
+const NUM_CIRCLES: u32 = 32;
 
 pub struct CircleWrapper {
     pub circles: CirclesToDisplay,
@@ -17,8 +17,8 @@ pub fn main_fs(
     out: &mut Vec4,
     #[spirv(uniform, descriptor_set = 0, binding = 0)] circle_wrapper: &CircleWrapper,
 ) {
-    let mut final_out = vec4(0.0, 0.0, 0.0, 0.0);
     let circles = circle_wrapper.circles.circles;
+    let mut final_out = vec4(0.0, 0.0, 0.0, 0.0);
     let frag_coords = vec2(in_frag_coord.x, in_frag_coord.y);
     for i in 0..NUM_CIRCLES {
         let circle_pass = render_circle(frag_coords, circles[i as usize]);
@@ -28,7 +28,12 @@ pub fn main_fs(
 }
 
 pub fn render_circle(frag_coord: Vec2, circle: Circle) -> Vec4 {
-    let center = circle.pos;
+    let center = Vec2Homebrew {
+        x: circle.pos.x * 0.001,
+        y: circle.pos.y * 0.001,
+        z: 0.0,
+        w: 0.0,
+    };
     let radius_squared = 100.0;
     let dx = frag_coord.x - center.x;
     let dy = frag_coord.y - center.y;
